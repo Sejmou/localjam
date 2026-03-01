6e0cf101f7fa0c65548618cbf05be990c5502266
Sejmou <samo.kolter@gmail.com>
2026-03-01 20:08:17 +0100

    WIP running drizzle migrations w/ helper code from @proj-airi packages

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,11 @@
 import { drizzle } from 'drizzle-orm/pglite';
 import { PGlite } from '@electric-sql/pglite';
 import * as schema from './schema';
+import { migrate } from '@proj-airi/drizzle-orm-browser-migrator/pglite';
+import migrations from 'drizzle-migrations.sql';
 
 const client = new PGlite('idb://localjam-db');
 await client.waitReady;
-export const db = drizzle(client, { schema });
+const _db = drizzle(client, { schema });
+await migrate(_db, migrations);
+export const db = _db;
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import DrizzleORMMigrations from '@proj-airi/unplugin-drizzle-orm-migrations/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), DrizzleORMMigrations()],
 	optimizeDeps: {
 		exclude: ['@electric-sql/pglite']
 	}
