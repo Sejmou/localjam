60b8045be25aca900f471f62600b8faeaaf6fed3
Sejmou <samo.kolter@gmail.com>
2026-03-01 20:11:35 +0100

    cursor fixed applying drizzle migrations <3

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/pglite';
 import { PGlite } from '@electric-sql/pglite';
 import * as schema from './schema';
 import { migrate } from '@proj-airi/drizzle-orm-browser-migrator/pglite';
-import migrations from 'drizzle-migrations.sql';
+import migrations from 'virtual:drizzle-migrations.sql';
 
 const client = new PGlite('idb://localjam-db');
 await client.waitReady;
